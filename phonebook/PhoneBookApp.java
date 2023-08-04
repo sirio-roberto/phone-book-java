7f6aed0ed1248ef8fec706d3d69f28d8469e71f8
Sirio <sirioroberto1@hotmail.com>
2023-08-04 00:11:33 -0300

    Implemented bubble sort and method to save sorted rows to a file to run some tests later

--- phonebook/PhoneBookApp.java
+++ phonebook/PhoneBookApp.java
@@ -1,31 +1,91 @@
 package phonebook;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.time.Instant;
 import java.time.LocalTime;
 import java.time.ZoneId;
 
 public class PhoneBookApp {
+    // File with all contacts and phone numbers
     private final String DIRECTORY_FILE = "/tmp/phoneBookFiles/directory.txt";
-    private final String FIND_FILE = "/tmp/phoneBookFiles/find.txt";
+    // File with names to find
+    private final String FIND_FILE = "/tmp/phoneBookFiles/small_find.txt";
 
     public void run() {
         String[] findRows = getAllFileRows(FIND_FILE);
         String[] dirRows = getAllFileRows(DIRECTORY_FILE);
+
         if (findRows != null && dirRows != null) {
-            System.out.println("Start searching...");
-
-            long startTime = System.currentTimeMillis();
-            int foundEntries = findEntries(findRows, dirRows);
-            long endTime = System.currentTimeMillis();
-
-            System.out.printf("Found %s / %s entries. Time taken: %s\n",
-                    foundEntries, findRows.length, getTimeTakenStr(startTime, endTime));
+            runLinearSearch(findRows, dirRows);
+            System.out.println();
+            runBubbleSortAndJumpSearch(findRows, dirRows);
         } else {
             System.out.println("Error while processing files!");
         }
+    }
+
+    private void runBubbleSortAndJumpSearch(String[] findRows, String[] dirRows) {
+        String[] sortedDirRows = dirRows.clone();
+
+        long startTime = System.currentTimeMillis();
+        runBubbleSort(sortedDirRows);
+        long endTime = System.currentTimeMillis();
+
+        saveAllRowsToFile(sortedDirRows, DIRECTORY_FILE, "_sorted");
+
+        System.out.printf("Sorting time: %s\n", getTimeTakenStr(startTime, endTime));
+    }
+
+    private void saveAllRowsToFile(String[] rowsArray, String fileName, String fileSuffix) {
+        String newFileName;
+        // has extension set
+        if (fileName.indexOf(".") > 0) {
+            String currentFileName = fileName.substring(0, fileName.lastIndexOf("."));
+            String fileExtension = fileName.substring(fileName.lastIndexOf("."));
+            newFileName = currentFileName.concat(fileSuffix).concat(fileExtension);
+        } else {
+            newFileName = fileName.concat(fileSuffix);
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(newFileName))) {
+            for (String row: rowsArray) {
+                writer.write(row);
+                writer.write("\n");
+            }
+        } catch (IOException ex) {
+            System.out.println("Error while writing to file: " + newFileName);
+        }
+    }
+
+    private void runBubbleSort(String[] unsortedDirRows) {
+        for (int i = 0; i < unsortedDirRows.length; i++) {
+            boolean hasChanged = false;
+            for (int j = 0; j < unsortedDirRows.length - 1; j++) {
+                int startNameIndex1 = unsortedDirRows[j].indexOf(" ") + 1;
+                int startNameIndex2 = unsortedDirRows[j + 1].indexOf(" ") + 1;
+                String name1 = unsortedDirRows[j].substring(startNameIndex1);
+                String name2 = unsortedDirRows[j + 1].substring(startNameIndex2);
+                if (name1.compareTo(name2) > 0) {
+                    String aux = unsortedDirRows[j];
+                    unsortedDirRows[j] = unsortedDirRows[j + 1];
+                    unsortedDirRows[j + 1] = aux;
+                    hasChanged = true;
+                }
+            }
+            if (!hasChanged) {
+                return;
+            }
+        }
+    }
+
+    private void runLinearSearch(String[] findRows, String[] dirRows) {
+        System.out.println("Start searching (linear search)...");
+
+        long startTime = System.currentTimeMillis();
+        int foundEntries = findEntriesUsingLinearSearch(findRows, dirRows);
+        long endTime = System.currentTimeMillis();
+
+        System.out.printf("Found %s / %s entries. Time taken: %s\n",
+                foundEntries, findRows.length, getTimeTakenStr(startTime, endTime));
     }
 
     private String getTimeTakenStr(long startTime, long endTime) {
@@ -38,7 +98,7 @@ public class PhoneBookApp {
                 localTime.getNano() / 1000000);
     }
 
-    private int findEntries(String[] findRows, String[] dirRows) {
+    private int findEntriesUsingLinearSearch(String[] findRows, String[] dirRows) {
         int foundEntries = 0;
         for (String fRow: findRows) {
             for (String dRow: dirRows) {
